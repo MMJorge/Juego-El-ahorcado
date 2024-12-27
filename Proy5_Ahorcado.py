6e15b7e841efb4ba74ef285d3a7e0d878f49f4c2
MMJorge <jorgemmunin@gmail.com>
2024-12-27 12:58:10 +0100

    Se agrega el juego

---
+++ Proy5_Ahorcado.py
@@ -0,0 +1,37 @@
+from random import *
+
+nombre = input('Por favor, dime tu nombre: ')
+print(f'Está bien {nombre}. Vamos a jugar al ahorcado. Estás preparado?')
+
+lista_palabras = ['ahumado','salmon','colorante','tinta','entrecot','jirafa','panel','sardina','bahía']
+palabra = choice(lista_palabras)
+rayas = len(palabra)
+print(f'La palabra secreta tiene {rayas} letras')
+
+#imprimir espacios
+
+def imprimir_rayas(lista_rayas):
+    for n in lista_rayas:
+        n == rayas
+        return lista_rayas[n]
+
+imprimir_rayas('_','__','___','____','_____','______','_______','________')
+
+#elige una letra
+def elige_letra():
+    vida = 6
+    eleccion = input('Por favor, elige una letra del abecedario')
+    while eleccion not in palabra:
+        vida -= 1
+    return eleccion
+    else
+
+
+    lista = []
+    vidas = 6
+    intentos = 0
+    for n in lista:
+        letra.append(lista)
+        if letra ==
+            return
+
